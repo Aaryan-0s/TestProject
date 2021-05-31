721f203bb9ad49573af47116414ab8977c8f4bfe
User <aaryanbasnet16@gmail.com>
2021-05-31 12:12:35 +0545

    area of a triangle

---
+++ Q2.py
@@ -0,0 +1,5 @@
+''' Write a program that reads the length and base and height of the right angle triangle and finds the area'''
+b=int(input("enter the base of the triangle:"))
+h=int(input("enter the height of the triangle is : "))
+area=1/2*(b*h)
+print(f"the area of the triangleis : {area}")
