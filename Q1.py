7e8095e0e72ff38ef8dc3fd62b2c40e2749d3aef
User <aaryanbasnet16@gmail.com>
2021-05-31 10:20:08 +0545

    the sum of 3 number

---
+++ Q1.py
@@ -0,0 +1,5 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enterthe first number:"))
+num3=int(input("Enter the first number:"))
+sum=num1+num2+num3
+print(f"the sum of the 3 number is {sum}")
